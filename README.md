11ca1b79781dcebfdfb98de77bd5c816fbd34290
Antoine <geekshadow@gmail.com>
2012-08-20 10:23:12 +0200

    First commit, yay !

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 nightingale-tools
 =================
+
+This repo is a set of tools for Nightingale.
+
+buildbot: it's a bash script to compile Nightingale if there are changes on the repo.
+You have to configure it yourself by editing it, future versions will be better ;)
+You can put an entry in cron to build everyday.
+
+TODO :
+  * Make the script work on Windows and Mac OS X
+  * On Windows we need to provide .zip/.exe (builded with InnoSetup)
+  * The script should also be able to build public releases
+
---
+++ buildbot/buildbot_ng.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Check the architecture platform (eg. i686)
+arch=`uname -m`
+# The branch to build
+branch='nightingale-1.11'
+# Version of Nightingale
+version='1.11.1'
+ngalebuild=`date +%Y-%m-%d-%H-%M-%S`
+# Sourceforge.net username
+sfnet='geekshadow'
+# rsync binary (rsyncp allow password to be store
+# insted of using public/private keys)
+rsync='/home/serveur/./rsyncp'
+
+# Going to the repo folder
+cd ~/dev/nightingale-hacking
+# Checkout the right branch
+git checkout ${branch}
+# Fetch changes from GitHub
+git fetch origin
+# Merge
+ngalechange=`git merge origin/${branch}`
+
+# If there are new changes, let's build !
+if [ "$ngalechange" != 'Already up-to-date.' ]
+ then
+
+make -f nightingale.mk clobber
+
+./build.sh > buildlog
+ngalebuildstatus=`cat buildlog | tail --lines=1`
+
+# If ngale was successfully builded, package it
+if [ "$ngalebuildstatus" = 'Build finished!' ]
+ then
+
+mv compiled/dist compiled/Nightingale
+cd compiled
+#Tar then bz2
+tar cvf nightingale-${version}_linux-${arch}.tar Nightingale
+bzip2 nightingale-${version}_linux-${arch}.tar
+#Making a md5 sum
+md5sum nightingale-${version}_linux-${arch}.tar.bz2 > nightingale-${version}_linux-${arch}.tar.bz2.md5
+#Creating a folder and moving the file to be reachable
+mkdir /var/www/ngale/$ngalebuild
+mkdir /var/www/ngale/$ngalebuild/addons
+mv nightingale-${version}_linux-${arch}.tar.bz2* /var/www/ngale/$ngalebuild
+mv xpi-stage/7digital/*.xpi /var/www/ngale/$ngalebuild/addons
+mv xpi-stage/albumartlastfm/*.xpi /var/www/ngale/$ngalebuild/addons
+mv xpi-stage/audioscrobbler/*.xpi /var/www/ngale/$ngalebuild/addons
+mv xpi-stage/concerts/*.xpi /var/www/ngale/$ngalebuild/addons
+mv xpi-stage/mashTape/*.xpi /var/www/ngale/$ngalebuild/addons
+mv xpi-stage/shoutcast-radio/*.xpi /var/www/ngale/$ngalebuild/addons
+
+#Uploading on sourceforge.net
+${rsync} -e ssh /var/www/ngale/$ngalebuild ${sfnet}@frs.sourceforge.net:/home/pfs/project/ngale/1.11-Nightlies/GNU-Linux/${arch} -r --progress
+
+ else
+echo "Build fail, see buildlog for more info"
+fi
+
+ else
+echo "Nothing to do, bye !"
+fi
+
