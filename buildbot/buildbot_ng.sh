c62e8664ff07949d8ee5ca838c790dc52ab7f45d
Antoine <geekshadow@gmail.com>
2012-08-21 17:17:17 +0200

    Improve script with a config file

--- buildbot/buildbot_ng.sh
+++ buildbot/buildbot_ng.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
+source config.sh
 # Check the architecture platform (eg. i686)
 arch=`uname -m`
-# The branch to build
-branch='nightingale-1.11'
-# Version of Nightingale
-version='1.11.1'
 ngalebuild=`date +%Y-%m-%d-%H-%M-%S`
 daybefore=`date +%Y-%m-%d --date '1 days ago'`
-# Sourceforge.net username
-sfnet='geekshadow'
-# rsync binary (rsyncp allow password to be store
-# insted of using public/private keys)
-rsync='/home/serveur/./rsyncp'
-
 # Going to the repo folder
-cd ~/dev/nightingale-hacking
+cd "${repo}"
 # Checkout the right branch
 git checkout ${branch}
 # Fetch changes from GitHub
@@ -40,24 +31,24 @@ mv compiled/dist compiled/Nightingale
 cd compiled
 git log --after={${daybefore}} > changes.txt
 #Tar then bz2
-tar cvf nightingale-${version}_linux-${arch}.tar Nightingale
-bzip2 nightingale-${version}_linux-${arch}.tar
+tar cvf nightingale-${version}_${osname}-${arch}.tar Nightingale
+bzip2 nightingale-${version}_${osname}-${arch}.tar
 #Making a md5 sum
-md5sum nightingale-${version}_linux-${arch}.tar.bz2 > nightingale-${version}_linux-${arch}.tar.bz2.md5
+md5sum nightingale-${version}_${osname}-${arch}.tar.bz2 > nightingale-${version}_${osname}-${arch}.tar.bz2.md5
 #Creating a folder and moving the file to be reachable
-mkdir /var/www/ngale/$ngalebuild
-mkdir /var/www/ngale/$ngalebuild/addons
-mv nightingale-${version}_linux-${arch}.tar.bz2* /var/www/ngale/$ngalebuild
-mv changes.txt /var/www/ngale/$ngalebuild
-mv xpi-stage/7digital/*.xpi /var/www/ngale/$ngalebuild/addons
-mv xpi-stage/albumartlastfm/*.xpi /var/www/ngale/$ngalebuild/addons
-mv xpi-stage/audioscrobbler/*.xpi /var/www/ngale/$ngalebuild/addons
-mv xpi-stage/concerts/*.xpi /var/www/ngale/$ngalebuild/addons
-mv xpi-stage/mashTape/*.xpi /var/www/ngale/$ngalebuild/addons
-mv xpi-stage/shoutcast-radio/*.xpi /var/www/ngale/$ngalebuild/addons
+mkdir $compiled/$ngalebuild
+mkdir $compiled/$ngalebuild/addons
+mv nightingale-${version}_${osname}-${arch}.tar.bz2* $compiled/$ngalebuild
+mv changes.txt $compiled/$ngalebuild
+mv xpi-stage/7digital/*.xpi $compiled/$ngalebuild/addons
+mv xpi-stage/albumartlastfm/*.xpi $compiled/$ngalebuild/addons
+mv xpi-stage/audioscrobbler/*.xpi $compiled/$ngalebuild/addons
+mv xpi-stage/concerts/*.xpi $compiled/$ngalebuild/addons
+mv xpi-stage/mashTape/*.xpi $compiled/$ngalebuild/addons
+mv xpi-stage/shoutcast-radio/*.xpi $compiled/$ngalebuild/addons
 
 #Uploading on sourceforge.net
-${rsync} -e ssh /var/www/ngale/$ngalebuild ${sfnet}@frs.sourceforge.net:/home/pfs/project/ngale/1.11-Nightlies/GNU-Linux/${arch} -r --progress
+${rsync} -e ssh $compiled/$ngalebuild ${sfnet}@frs.sourceforge.net:/home/pfs/project/ngale/1.11-Nightlies/GNU-${osname}/${arch} -r --progress
 
  else
 echo "Build fail, see buildlog for more info"
@@ -66,4 +57,3 @@ fi
  else
 echo "Nothing to do, bye !"
 fi
-
---
+++ buildbot/config-sample.sh
@@ -0,0 +1,16 @@
+# OS
+osname='linux'
+# The git branch you want to compile
+branch='nightingale-1.11'
+# The Nightingale version
+version='1.11.1'
+# Your sourceforge.net username
+sfnetuser='yoursfuser'
+# Your sourceforge.net password
+sfnetpass='yoursfpass'
+# The rsync binary/script
+rsync='/home/serveur/./rsyncp.sh'
+# The repo folder
+repo='/home/serveur/dev/nightingale-hacking'
+# The local path to store compiled files
+compiled='/var/www/ngale'
