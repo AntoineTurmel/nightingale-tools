8f8018045612e20c61218d339c026684a45c4a2a
Antoine <geekshadow@gmail.com>
2012-10-30 17:13:30 +0100

    add build number and put in the same folder, fix #6

--- buildbot/buildbot_ng.sh
+++ buildbot/buildbot_ng.sh
@@ -2,7 +2,7 @@
 source config.sh
 # Check the architecture platform (eg. i686)
 arch=`uname -m`
-ngalebuild=`date +%Y-%m-%d-%H-%M-%S`
+ngalebuild=`date +%Y-%m-%d`
 daybefore=`date +%Y-%m-%d --date '1 days ago'`
 # Going to the repo folder
 cd "${repo}"
@@ -18,6 +18,9 @@ ngalechange=`git merge origin/${branch}`
 if [ "$ngalechange" != 'Already up-to-date.' ] || [ "$1" = "-f" ]
  then
 
+# Get the buildnumber
+buildnumber=`cat build/sbBuildInfo.mk.in | grep BuildNumber= | sed -e 's/BuildNumber=//g'`
+
 make -f nightingale.mk clobber
 
 ./build.sh > buildlog
@@ -31,14 +34,14 @@ mv compiled/dist compiled/Nightingale
 cd compiled
 git log --after={${daybefore}} > changes.txt
 #Tar then bz2
-tar cvf nightingale-${version}_${osname}-${arch}.tar Nightingale
-bzip2 nightingale-${version}_${osname}-${arch}.tar
+tar cvf nightingale-${version}-${buildnumber}_${osname}-${arch}.tar Nightingale
+bzip2 nightingale-${version}-${buildnumber}_${osname}-${arch}.tar
 #Making a md5 sum
-md5sum nightingale-${version}_${osname}-${arch}.tar.bz2 > nightingale-${version}_${osname}-${arch}.tar.bz2.md5
+md5sum nightingale-${version}-${buildnumber}_${osname}-${arch}.tar.bz2 > nightingale-${version}-${buildnumber}_${osname}-${arch}.tar.bz2.md5
 #Creating a folder and moving the file to be reachable
 mkdir $compiled/$ngalebuild
 mkdir $compiled/$ngalebuild/addons
-mv nightingale-${version}_${osname}-${arch}.tar.bz2* $compiled/$ngalebuild
+mv nightingale-${version}-${buildnumber}_${osname}-${arch}.tar.bz2* $compiled/$ngalebuild
 mv changes.txt $compiled/$ngalebuild
 mv xpi-stage/7digital/*.xpi $compiled/$ngalebuild/addons
 mv xpi-stage/albumartlastfm/*.xpi $compiled/$ngalebuild/addons
@@ -48,7 +51,7 @@ mv xpi-stage/mashTape/*.xpi $compiled/$ngalebuild/addons
 mv xpi-stage/shoutcast-radio/*.xpi $compiled/$ngalebuild/addons
 
 #Uploading on sourceforge.net
-${rsync} -e ssh $compiled/$ngalebuild ${sfnetuser}@frs.sourceforge.net://home//pfs//project//ngale//1.11-Nightlies//${osname}//${arch} -r --progress
+${rsync} -e ssh $compiled/$ngalebuild ${sfnetuser}@frs.sourceforge.net://home//pfs//project//ngale//1.11-Nightlies -r --progress
 
  else
 echo "Build fail, see buildlog for more info"
