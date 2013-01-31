66fc0a564baf294693c035f34befb251fae76315
Matt Parnell <parwok@gmail.com>
2013-01-31 02:10:04 -0600

    some cleanup on the formatting and fix a character issue that mac seemed to create...freaking apple, again

--- buildbot/config-sample.sh
+++ buildbot/config-sample.sh
@@ -1,8 +1,11 @@
 # The git branch you want to compile
 branch='sb-trunk-oldxul'
+
 # Your sourceforge.net username
 sfnetuser='yoursfuser'
+
 # The repo folder
 repo='/home/serveur/dev/nightingale-hacking'
+
 # The local path to store compiled files
 compiled='/var/www/ngale'
