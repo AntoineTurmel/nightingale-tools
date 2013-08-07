7d927c793950c2d3b4a304807a98914538194cd9
Antoine Turmel <geekshadow@gmail.com>
2013-08-07 02:37:13 +0200

    script to fetch missing langpacks

---
+++ l10n/fetch-missing.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sb_version="2.1.0" #latest on fly compiled langpack
+sb_user_agent='Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.2.3) Gecko/20101201 Songbird/'$sb_version
+
+# Path of songbird-l10n
+l10n_path='/home/antoine/Dev/songbird-l10n'
+
+cd $l10n_path
+
+# Makes the list of missing locales
+diff all-locales shipped-locales | grep "<" | cut -d' ' -f2 > missing-locales
+
+cd locales
+
+for lang_code in $(cat $l10n_path/missing-locales)
+do
+
+wget -O "langpack-"$lang_code"-"$sb_version".xpi" http://translate.songbirdnest.com/languages/$lang_code/langpack --user-agent="$sb_user_agent"
+wait 60
+
+done
+
+cd ..
+rm missing-locales
